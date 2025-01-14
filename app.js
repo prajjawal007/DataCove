9fd32b496af6f3d6c58ac9d481124b83c0bd76ff
Prajjawal Agrawal <144593090+prajjawal007@users.noreply.github.com>
2025-01-14 15:40:47 +0530

    Setup Vercel

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
-const express= require("express");
+// const express= require("express");
+// const userRouter = require("./routes/user.routes");
+
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// const connectToDB = require("./config/db");
+// connectToDB();
+
+// const cookieParser = require("cookie-parser");
+
+// const indexRouter = require("./routes/index.route");
+
+// const app = express();
+// const port = 3000;
+
+
+// app.use(cookieParser());
+
+// app.use(express.json());
+// app.use(express.urlencoded({extended:true}));
+
+// app.set("view engine","ejs");
+
+// app.use('/',indexRouter);
+// app.use('/user',userRouter);
+
+// app.listen(port,()=>{
+//     console.log(`Server is Running on ${port}`);
+// })
+const express = require("express");
 const userRouter = require("./routes/user.routes");
 
 const dotenv = require("dotenv");
@@ -12,19 +42,15 @@ const cookieParser = require("cookie-parser");
 const indexRouter = require("./routes/index.route");
 
 const app = express();
-const port = 3000;
-
 
 app.use(cookieParser());
-
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-app.use('/',indexRouter);
-app.use('/user',userRouter);
+app.use("/", indexRouter);
+app.use("/user", userRouter);
 
-app.listen(port,()=>{
-    console.log(`Server is Running on ${port}`);
-})
+// Export the Express app as a module
+module.exports = app;
